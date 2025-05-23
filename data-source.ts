a1c9c6ce1569c938320bd4fc2cf4f86917348e90
Fabio Texeira Mourão <fabio.t.mourao@gmail.com>
2025-05-22 22:53:12 -0300

    ajustes

--- data-source.ts
+++ data-source.ts
@@ -18,6 +18,7 @@ export const AppDataSource = new DataSource({
         username: process.env.DATABASE_USER,
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_NAME,
+        ssl: { rejectUnauthorized: false },
       }),
   entities: [Product, Order, OrderItem],
   migrations: ['src/migrations/*.ts'],
