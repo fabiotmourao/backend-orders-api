6097a04369b6354070b69a0b4fedfcc8b59506f9
Fabio Texeira Mourão <fabio.t.mourao@gmail.com>
2025-05-22 23:34:53 -0300

    up

--- data-source.ts
+++ data-source.ts
@@ -4,22 +4,10 @@ import { Product } from './src/products/entities/product.entity';
 import { Order } from './src/orders/entities/order.entity';
 import { OrderItem } from './src/orders/entities/order-item.entity';
 
-const isRender = !!process.env.DATABASE_URL;
-
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  ...(isRender
-    ? {
-        url: process.env.DATABASE_URL,
-        ssl: { rejectUnauthorized: false },
-      }
-    : {
-        host: process.env.DATABASE_HOST,
-        port: Number(process.env.DATABASE_PORT),
-        username: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_NAME,
-      }),
+  url: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
   entities: [Product, Order, OrderItem],
   migrations: ['src/migrations/*.ts'],
   synchronize: false,
